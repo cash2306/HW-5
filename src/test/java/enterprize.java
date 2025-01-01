7208c698056531b8cb3fc142cd86926fbb74c90b
cash2306 <Yablochkinaea@mail.ru>
2025-01-01 18:49:26 +0300

    add enterprize

---
+++ src/test/java/enterprize.java
@@ -0,0 +1,24 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class enterprize {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void openEnterprisesPageTest(){
+        open("https://github.com");
+        $(byTagAndText("button", "Solutions")).hover();
+        $(byTagAndText("a", "Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+    }
+}
